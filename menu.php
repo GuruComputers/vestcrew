9206b626224870b31f443a66a05249b36967dc2e
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-29 17:36:06 +0000

    spacing row added

--- menu.php
+++ menu.php
@@ -8,6 +8,10 @@
 		<td width="50%" align="center"><a href="./peter.php"><img src="./img/petertimevideos_text.png" class="persontxt"></a></td>
 	</tr>
 	<tr>
+		<td width="50%" height="10"></td>
+		<td width="50%" height="10"></td>
+	</tr>
+	<tr>
 		<td width="50%" align="center"><a href="./boristime.php"><img src="./img/boristime.png" class="person"></a></td>
 		<td width="50%" align="center"><a href="./persian.php"><img src="./img/parsa29801.png" class="person"></a></td>
 	</tr>
