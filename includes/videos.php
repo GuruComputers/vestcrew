497f678a9ba786274bf15fe8fd509a8f3b85448b
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-29 15:44:15 +0000

    videos.php Test 4

--- includes/videos.php
+++ includes/videos.php
@@ -1,9 +1,10 @@
-<script>
+<script type="text/javascript" src="../src/ytv.js"></script>
+		<script>
 			window.onload = function(){
 				
 				
 				window.controller = new YTV('frame', {
-					user: '<?php echo $username ?>',
+					user: '<?php echo $username; ?>',
 					accent: 'yellow'
 				});
 		
