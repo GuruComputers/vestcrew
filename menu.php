52e8e80dcb2b0195568608a8a5ee32a7cc1d4119
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-29 17:26:31 +0000

    a elements added to menu

--- menu.php
+++ menu.php
@@ -1,19 +1,19 @@
 <table width="930">
 	<tr>
-		<td width="50%" align="center"><img src="./img/izroderrz.png" class="person"></td>
-		<td width="50%" align="center"><img src="./img/petertimevideos.png" class="person"></td>
+		<td width="50%" align="center"><a href="./izroderrz.php"><img src="./img/izroderrz.png" class="person"></a></td>
+		<td width="50%" align="center"><a href="./peter.php"><img src="./img/petertimevideos.png" class="person"></a></td>
 	</tr>
 	<tr>
-		<td width="50%" align="center"><img src="./img/izroderrz_text.png" class="persontxt"></td>
-		<td width="50%" align="center"><img src="./img/petertimevideos_text.png" class="persontxt"></td>
+		<td width="50%" align="center"><a href="./izroderrz.php"><img src="./img/izroderrz_text.png" class="persontxt"></a></td>
+		<td width="50%" align="center"><a href="./peter.php"><img src="./img/petertimevideos_text.png" class="persontxt"></a></td>
 	</tr>
 	<tr>
-		<td width="50%" align="center"><img src="./img/boristime.png" class="person"></td>
-		<td width="50%" align="center"><img src="./img/parsa29801.png" class="person"></td>
+		<td width="50%" align="center"><a href="./boristime.php"><img src="./img/boristime.png" class="person"></a></td>
+		<td width="50%" align="center"><a href="./persian.php"><img src="./img/parsa29801.png" class="person"></a></td>
 	</tr>
 	<tr>
-		<td width="50%" align="center"><img src="./img/boristime_text.png" class="persontxt"></td>
-		<td width="50%" align="center"><img src="./img/parsa29801_text.png" class="persontxt"></td>
+		<td width="50%" align="center"><a href="./boristime.php"><img src="./img/boristime_text.png" class="persontxt"></a></td>
+		<td width="50%" align="center"><a href="./persian.php"><img src="./img/parsa29801_text.png" class="persontxt"></a></td>
 	</tr>
 </table>
 
