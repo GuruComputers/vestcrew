4795a5e02acd82b7c54be70e241f502500421977
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-29 15:19:33 +0000

    de-capitalize

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <table width="930" border="1">
 	<tr>
 		<td width="50%"><img src="./img/izroderrz.png" class="person"></td>
-		<td width="50%"><img src="./img/PeterTimeVideos.png" class="person"></td>
+		<td width="50%"><img src="./img/petertimevideos.png" class="person"></td>
 	</tr>
 	<tr>
 		<td width="50%"><img src="./img/izroderrz_text.png" class="persontxt"></td>
 		<td width="50%"><img src="./img/petertimevideos_text.png" class="persontxt"></td>
 	</tr>
 	<tr>
-		<td width="50%"><img src="./img/BorisTime.png" class="person"></td>
+		<td width="50%"><img src="./img/boristime.png" class="person"></td>
 		<td width="50%"><img src="./img/parsa29801.png" class="person"></td>
 	</tr>
 	<tr>
