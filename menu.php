393931961d0b3c1838750b383f13dbbccd803679
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-29 14:55:44 +0000

    table contents added

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <table width="624" border="1">
 	<tr>
-		<td width="25%">person 1</td>
-		<td width="25%">person 2</td>
-		<td width="25%">person 3</td>
-		<td width="25%">person 4</td>
+		<td width="25%"><img src="./img/izroderrz.png" class="person"></td>
+		<td width="25%"><img src="./img/PeterTimeVideos.png" class="person"></td>
+		<td width="25%"><img src="./img/BorisTime.png" class="person"></td>
+		<td width="25%"><img src="./img/parsa29801.png" class="person"></td>
 	</tr>
 	<tr>
-		<td width="25%">name 1</td>
-		<td width="25%">name 2</td>
-		<td width="25%">name 3</td>
-		<td width="25%">name 4</td>
+		<td width="25%"><img src="./img/izroderrz_text.png" class="persontxt"></td>
+		<td width="25%"><img src="./img/petertimevideos_text.png" class="persontxt"></td>
+		<td width="25%"><img src="./img/boristime_text.png" class="persontxt"></td>
+		<td width="25%"><img src="./img/parsa29801_text.png" class="persontxt"></td>
 	</tr>
 </table>
