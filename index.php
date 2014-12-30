36ac5decb299adcea09895f331926f1321637e66
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-30 12:04:27 +0000

    peter twitch added

--- index.php
+++ index.php
@@ -47,7 +47,9 @@
 				<a href="https://www.youtube.com/user/PeterTimeVideos" alt="youtube link to PeterTimeVideos"><img src="./img/youtube.png" alt="youtube icon" class="youtube">PeterTimeVideos</a>
 				<a href="https://www.youtube.com/channel/UCs2f28VuqTUA6wXhfki6nWA" alt="youtube link to BorisTime"><img src="./img/youtube.png" alt="youtube icon" class="youtube">BorisTime</a>
 				<a href="https://www.youtube.com/channel/UCNiDwRbFSbOdmVbLbrl4Evw" alt="youtube link to ThePersianGamer"><img src="./img/youtube.png" alt="youtube icon" class="youtube">ThePersianGamer</a>
-				<a href="http://www.twitch.tv/izroderrzontwitch" alt="twitch link to IzRoderrz"><img src="./img/twitch.png" alt="twitch icon" class="twitch" title="IzRoderrz"></a>
+				<br />
+				<a href="http://www.twitch.tv/izroderrzontwitch" alt="twitch link to IzRoderrz"><img src="./img/twitch.png" alt="twitch icon" class="twitch">IzRoderrz</a>
+				<a href="http://www.twitch.tv/petertime" alt="twitch link to PeterTimeVideos"><img src="./img/twitch.png" alt="twitch icon" class="twitch">PeterTimeVideos</a>
 				<br />
 				<br />
 				<?php
